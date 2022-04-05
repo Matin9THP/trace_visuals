8c19cc07a9b2615e3c2bd9388644673f9ec2fad5
Matin Tahmasebpour <89160702+Matin9THP@users.noreply.github.com>
2022-04-05 16:33:44 -0400

    Add files via upload

---
+++ Pytorch_visuals/data/align_time.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def main():
+    # Obtained from previous analysis (see align_time_README.md)
+    ref_ts = 1802598649099327
+    # Add 5h to convert from EST to UTC
+    ref_t = np.datetime64("2022-03-31T12:05:09.000000000") + np.timedelta64(5, "h")
+
+    traces = ["bio", "vfs_rw", "open"]
+    # traces = ["open"]
+
+    for trace in traces:
+        infile = open(trace + ".out", "r")
+        outfile = open(trace + "_time_aligned.out", "w")
+
+        # Discard the first 2 lines of traces
+        infile.readline()
+        infile.readline()
+        
+
+        for line in infile:
+            cols = " ".join(line.split()).split(" ")
+            # Handle empty lines
+            if cols[0] == "":
+                continue
+            # Get the timestamp
+            ts = int(cols[0])
+            # Calculate the diff
+            ts_delta = ts - ref_ts
+            # Get the UTC time
+            t = ref_t + ts_delta
+            outfile.write(np.datetime_as_string(t) + " " + " ".join(cols[2::]) + "\n")
+
+
+if __name__ == "__main__":
+    main()
